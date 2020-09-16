e2ce4dc76cfa1ef6b1dc0a9e1af8cf5cf6aa5b68
Henrique Mendes <henrique.gmendes@hotmail.com>
2020-09-16 19:07:29 -0300

    add '/dashboard' main area table and empty URLs view | add relationship between URL and User collections

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const Url = require('../models/Url');
+const User = require('../models/User');
 
 const router = express.Router();
 
-router.get('/dashboard', (request, response) => {
-  response.render('dashboard');
+router.get('/dashboard', async (request, response) => {
+  try {
+    const data = await Url.find({ owner: '5f6152c92bbf27178936d42e' }).populate('owner');
+  
+    response.render('dashboard', { data, owner: data[0].owner });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post('/url/create', async (request, response) => {
@@ -18,6 +25,7 @@ router.post('/url/create', async (request, response) => {
     redirectUrl: protocol + redirectUrl,
     shortUrl: 'http://localhost:3000/s',
     expirationDateMs: dateMs,
+    owner:'5f6152c92bbf27178936d42e',
   });
 
   newUrl.shortUrl += '/' + newUrl._id;
